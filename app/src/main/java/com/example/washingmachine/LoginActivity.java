99bc5c42f2a03d33f2c4ae648023e03667a0ffb9
sin <tkdyr2>
2019-05-10 21:42:32 +0900

    로그인 화면 디자인 변경

--- app/src/main/java/com/example/washingmachine/LoginActivity.java
+++ app/src/main/java/com/example/washingmachine/LoginActivity.java
@@ -1,29 +1,27 @@
 package com.example.washingmachine;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class LoginActivity  extends Activity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button startbtn= (Button)findViewById(R.id.startbutton);
-        startbtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.login);
+        Button loginbtn = (Button) findViewById(R.id.loginbutton);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),
-                        LoginActivity.class);
+                        MenuActivity.class);
                 startActivity(intent);
             }
         });
 
     }
-
-
-
 }
