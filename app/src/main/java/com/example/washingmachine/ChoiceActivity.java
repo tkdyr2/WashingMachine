37ab1936179ad9844791a1acf3dd83c25d001543
sin <tkdyr2>
2019-05-10 22:28:19 +0900

    나의세탁기 선택 기초 화면 추가

---
+++ app/src/main/java/com/example/washingmachine/ChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.example.washingmachine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ChoiceActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choicemachine);
+    }
+
+}
--- app/src/main/java/com/example/washingmachine/MenuActivity.java
+++ app/src/main/java/com/example/washingmachine/MenuActivity.java
@@ -1,18 +1,25 @@
 package com.example.washingmachine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity  extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
-
+        Button btn1= (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ChoiceActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
