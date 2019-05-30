e9c2e6a1a0161da06bf9ecbd0084b02c7225fb70
sin <tkdyr2>
2019-05-31 01:34:03 +0900

    세탁기 어플 icon 디자인 추가

--- app/src/main/java/com/example/washingmachine/MyWashingMachineActivity.java
+++ app/src/main/java/com/example/washingmachine/MyWashingMachineActivity.java
@@ -1,7 +1,10 @@
 package com.example.washingmachine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyWashingMachineActivity  extends Activity {
 
@@ -9,7 +12,23 @@ public class MyWashingMachineActivity  extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mywashingmachine);
 
-    }
 
+        Button okbtn = (Button) findViewById(R.id.okbtn);
+
+
+        okbtn.setOnClickListener(new View.OnClickListener() {            //마이세탁기화면 확인버튼 누를시 메뉴화면으로 돌아감
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
 
 }
