89287844b6b2930af3993648de910515a0899b32
HOME-PC\owner <moriyoshe0825@gmail.com>
2019-05-31 16:52:05 +0900

    MeueActivity OK

--- app/src/main/java/com/example/washingmachine/MakeNotice.kt
+++ app/src/main/java/com/example/washingmachine/MakeNotice.kt
@@ -1,12 +1,25 @@
 package com.example.washingmachine
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MakeNotice : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.make_notice)
+
+        val Cancell = findViewById<View>(R.id.cancellation2) as Button
+        Cancell.setOnClickListener {
+            val intent = Intent(
+                applicationContext,
+                LookNoticeWhole::class.java
+            )
+            startActivity(intent)
+        }
+
     }
 }
